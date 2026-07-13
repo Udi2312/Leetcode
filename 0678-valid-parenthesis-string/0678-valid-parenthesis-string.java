03bc8d83e69d2a9d326e3c07297961bf353bc3da
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-07-13 11:12:18 +0530

    Time: 0 ms (100%), Space: 42.7 MB (68.96%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int min = 0, max = 0;
+      for(int i = 0; i<s.length(); i++){
+        if(s.charAt(i) == '('){
+            min++;
+            max++;
+        }
+        else if(s.charAt(i) == ')'){
+            max--;
+            min--;
+        }
+        else{
+            max++;
+            min--;
+        }
+        if(min < 0) min = 0;
+        if(max < 0) return false;
+      }
+      if(min==0) return true;
+      else return false;
+    }
+}
