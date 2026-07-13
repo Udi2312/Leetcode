26172bd582fff43a8e063463f1819e4bda5e1733
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-07-13 07:48:29 +0530

    Time: 2 ms (99.26%), Space: 72.4 MB (48.25%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        for(int i = 0; i<bills.length; i++){
+            if(bills[i] == 5) five++;
+            if(bills[i] == 10){
+                five--;
+                ten++;
+                if(five < 0) return false;
+            }
+            if(bills[i] == 20){
+                if(ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                }
+                else if(five >= 3){
+                    five = five -3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
