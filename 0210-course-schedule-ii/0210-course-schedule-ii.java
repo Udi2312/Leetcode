5585e81bf7139132c8da4b0ff879122e3b3d3a42
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-08-10 07:36:55 +0530

    Time: 6 ms (42.39%), Space: 46.7 MB (80.75%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findOrder(int n, int[][] pre) {
+         List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i<n; i++) adj.add(new ArrayList<>());
+        int indegree[] = new int[n];
+        boolean vis[] = new boolean[n];
+        for(int i = 0; i<pre.length; i++){
+            int a = pre[i][0];
+            int b = pre[i][1];
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+            for(int i = 0; i<indegree.length; i++){
+                if(indegree[i] == 0){
+                    q.add(i);
+                    vis[i] = true;
+                }
+            }
+            while(q.size() > 0){
+                int front = q.remove();
+                ans.add(front);
+                for(int e: adj.get(front)){
+                    indegree[e]--;
+                    if(indegree[e]==0) {
+                        q.add(e);
+                        vis[e] = true;
+                    }
+                }
+            }
+            if(ans.size() < n) return new int[0];
+            int a[] = new int[ans.size()];
+            for(int i = 0; i<a.length; i++){
+                a[i] = ans.get(i);
+            }
+            return a;
+    }
+}
