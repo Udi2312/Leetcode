7b4083ab4375176b3cc3ca9c43b764b27bb150c7
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-08-14 00:22:54 +0530

    Time: 6 ms (85.5%), Space: 46.5 MB (70.29%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,56 @@
+class Solution {
+     class Triplet implements Comparable<Triplet>{
+        int node;
+        int cost;
+        int stops;
+        Triplet(int node, int cost, int stops){
+            this.node = node;
+            this.cost = cost;
+            this.stops = stops;
+        }
+        public int compareTo(Triplet t){
+            if(this.stops == t.stops) return this.cost-t.cost;
+            return this.stops -t.stops;
+        }
+    }
+    class Pair{
+            int node;
+            int price;
+            Pair(int node, int price){
+                this.node = node;
+                this.price = price;
+            }
+    }
+    public int findCheapestPrice(int n, int[][] edges, int src, int dst, int k) {
+         List<List<Pair>> adj = new ArrayList<>();
+        for(int i = 0; i<n; i++) adj.add(new ArrayList<>());
+         for(int i = 0; i<edges.length; i++){
+            int from = edges[i][0];
+            int to = edges[i][1];
+            int price = edges[i][2];
+            adj.get(from).add(new Pair(to,price));
+        }
+        int ans[] = new int[n];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[src] = 0;
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        pq.add(new Triplet(src,0,0));
+        while(pq.size() > 0){
+            Triplet top = pq.remove();
+            int node = top.node;
+            int cost = top.cost;
+            int stops = top.stops;
+            // if(node == dst) return ans[node];
+            if(stops == k+1) continue;
+            for(Pair p: adj.get(node)){
+                int totalCost = cost + p.price;
+                if(totalCost < ans[p.node]){
+                    ans[p.node] = totalCost;
+                    pq.add(new Triplet(p.node,totalCost, stops+1));
+                }
+            }
+        }
+        if(ans[dst] == Integer.MAX_VALUE) return -1;
+        return ans[dst];
+    }
+}
