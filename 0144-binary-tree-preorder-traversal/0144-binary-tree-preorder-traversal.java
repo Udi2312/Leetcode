122db075cc19b87cf600960b9bea414866011279
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-08-19 23:13:49 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.03%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,15 +14,15 @@
  * }
  */
 class Solution {
-    public void preorder(TreeNode root, List<Integer> ans){
+    public void inorder(TreeNode root, List<Integer> ans){
         if(root==null) return;
         ans.add(root.val);
-        preorder(root.left,ans);
-        preorder(root.right,ans);
+        inorder(root.left, ans);
+        inorder(root.right, ans);
     }
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        preorder(root,ans);
+        inorder(root,ans);
         return ans;
     }
 }
