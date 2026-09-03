fdd5bc1cce6a979fb26822a01aec5352c6ff3738
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-09-03 15:28:14 +0530

    Time: 52 ms (5.82%), Space: 48.5 MB (6.27%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+     static int dp[][];
+    public int lcs(StringBuilder a, int ai, StringBuilder b, int bi){
+        if(ai < 0 || bi < 0) return 0;
+        if(dp[ai][bi] != -1) return dp[ai][bi];
+        if(a.charAt(ai) == b.charAt(bi)) return dp[ai][bi] = 1 + lcs(a, ai-1, b, bi-1);
+        else{
+            return dp[ai][bi] = Math.max(lcs(a, ai-1, b, bi), lcs(a, ai, b, bi-1));
+        }
+    }
+    public int longestPalindromeSubseq(String s) {
+         dp = new int[s.length()][s.length()];
+
+        for(int[] arr: dp){
+            Arrays.fill(arr,-1);
+        }
+
+        StringBuilder x = new StringBuilder(s);
+        StringBuilder y = new StringBuilder(s).reverse();
+        return lcs(x, s.length()-1, y, s.length()-1);
+    }
+    public int minInsertions(String s) {
+        return s.length() - longestPalindromeSubseq(s);
+    }
+}
