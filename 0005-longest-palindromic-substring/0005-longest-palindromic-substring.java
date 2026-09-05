96c9a3c431d7711385f840d429b2856c815ba4a9
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-09-05 13:05:03 +0530

    Time: 235 ms (20.3%), Space: 112.6 MB (5.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,7 +1,8 @@
 class Solution {
     public String countSubstrings(String s) {
         int n = s.length();
-       List<String> ans = new ArrayList<>();
+        String ans = "";
+    //    List<String> ans = new ArrayList<>();
         int[][] dp = new int[n][n];
         for(int k = 0; k<n; k++){
             int i = 0;
@@ -9,19 +10,22 @@ class Solution {
             while(j<n){
                 if(i==j){
                     dp[i][j] = 1;
-                    ans.add(s.substring(i,j+1));
+                    ans = s.substring(i,j+1);
+                    // ans.add(s.substring(i,j+1));
                 }
                 else if(j==i+1){
                     if(s.charAt(i) == s.charAt(j)){
                         dp[i][j] = 1;
-                        ans.add(s.substring(i,j+1));
+                        ans = s.substring(i,j+1);
+                        // ans.add(s.substring(i,j+1));
                     } 
                 }
                 else{
                     if(s.charAt(i) == s.charAt(j)){
                         if(dp[i+1][j-1] == 1){
                             dp[i][j] = 1;
-                           ans.add(s.substring(i,j+1));
+                            ans = s.substring(i,j+1);
+                        //    ans.add(s.substring(i,j+1));
                         }
                     }
                 }
@@ -29,13 +33,13 @@ class Solution {
             }
 
         }
-        String l = "";
-        for(String a : ans){
-            if(a.length() > l.length()){
-                l = a;
-            }
-        }
-        return l;
+        // String l = "";
+        // for(String a : ans){
+        //     if(a.length() > l.length()){
+        //         l = a;
+        //     }
+        // }
+        return ans;
     }
     public String longestPalindrome(String s) {
         return countSubstrings(s);
