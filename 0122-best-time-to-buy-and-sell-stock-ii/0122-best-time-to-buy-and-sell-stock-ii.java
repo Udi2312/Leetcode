5f366b35841f503c382268d94c5236b7d049ce0e
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-09-09 18:23:51 +0530

    Time: 0 ms (100%), Space: 46.4 MB (48.45%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int pro  = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+                max = arr[i];
+                // pro = Math.max(pro,max-min);
+                continue;
+            }
+            if(arr[i] > max){
+                max = arr[i];
+                pro += max-min;
+                min = arr[i];
+                // max = Integer.MIN_VALUE;
+            }
+        }
+        return pro;
+    }
+}
