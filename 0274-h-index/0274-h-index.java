c4b324ca6ab72aed5ee31949c7559021ad47fc48
Udit Bansal <146841098+Udi2312@users.noreply.github.com>
2026-09-22 19:28:07 +0530

    Time: 7 ms (10.63%), Space: 43.7 MB (27.36%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int hIndex(int[] arr) {
+        Arrays.sort(arr);
+        int ans = arr[0];
+        int i = 1;
+        int j = 1000;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            int ct = 0;
+            for(int k = 0; k<arr.length; k++){
+                if(arr[k] >= mid) ct++;
+            }
+            if(ct >= mid) {
+                ans = mid;
+                i = mid +1;
+            }
+            else{
+                j = mid-1;
+            }
+        }
+        return ans;
+    }
+}
